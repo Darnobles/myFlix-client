ee711f753e807338f0ac8243fa92863df445a282
Darnobles <126102632+Darnobles@users.noreply.github.com>
2023-08-20 10:51:27 -0400

    refactor
    Replace hardcoded movies array with an empty array

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -1,40 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Wonder Women",
-      image: "https://upload.wikimedia.org/wikipedia/en/9/93/Wonder_Woman.jpg",
-      description:
-        "A pilot crashes on a mysterious island and one of the Amazonian women there goes with him back to England to help them fight in the war.",
-      genre: "Adventure",
-      director: "Patty Jenkins",
-    },
-
-    {
-      id: 2,
-      title: "The Rocketeer",
-      image: " ",
-      description:
-        "An action film is built around the main character being thrust into a series of events which involve voilence, explosions and  chases.",
-      genre: "Action",
-      director: "Joe Johnston",
-    },
-
-    {
-      id: 3,
-      title: "Dick Tracy",
-      image: " ",
-      description:
-        "Detective Dick Tracy is trying to find evidence to put the city's crime boss, Big Boy Caprice, away for good.",
-      genre: "Action",
-      director: "Warren Beatty",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch('https://github.com/Darnobles/movie_api.git')
+    .then((response) => response.json())
+    .then((data) => {
+      
+    })
+  })
 
   if (selectedMovie) {
     return (
