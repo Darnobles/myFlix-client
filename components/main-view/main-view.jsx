a3df5f183df07dbf7ce187fb33a86cd08abf133a
Darnobles <126102632+Darnobles@users.noreply.github.com>
2023-09-06 23:37:12 -0400

    refactor
    use React Router to enable routing and add NavBar

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -3,7 +3,10 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+import { NavigationBar } from "../navigation-bar/navigation-bar";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -44,40 +47,79 @@ export const MainView = () => {
   }, []);
 
 return (
-  <Row>
-    {!user ? (
-      <>
-        <LoginView onLoggedIn={(user, token) => {
-          setUser(user);
-          setToken(token);
-        }} />
-        or
-        <SignupView />
-      </>
-    ) : selectedMovie? (
-      <MovieView
-        movie={selectedMovie}
-        onBackClick={() => setSelectedMovie(null)}
-      />
-    ) : movies.length === 0 ? (
-      <div>The list is empty!</div>
-    ) : (
+  <BrowserRouter>
+    <NavigationBar
+      user={userName}
+      onLoggedOut={() => {
+        setUserName(null);
+        setToken(null);
+        localStorage.clear();
+      }}/>
+  <Row className="justify-content-md-center">
+    <Routes>
+      <Route
+        path="/signup" element={
+          <>
+            {user ? (
+              <Navigate to="/" />
+            ) : (
+              <Col md={5}>
+                <SignupView />
+              </Col>
+            )}
+          </>
+        }
+        />
+        <Route
+          path="/login" element={
+            <>
+              {user ? (
+                <Navigate to="/" />
+              ) : (
+                <Col md={5}>
+                  <LoginView onLoggedIn={(user) => setUser(user)} />
+                </Col>
+              )}
+            </>
+          }
+        />
+        <Route
+          path="/books/:bookId" element={
+            <>
+              {!user ? (
+                <Navigate to="/login" replace />
+              ) : movies.length === 0 ? (
+                <Col>The list is empty!</Col>
+              ) : (
+                <Col md={8}>
+                  <MovieView movies={movies} />
+                </Col>
+              )}
+            </>
+          }
+        />
+        <Route
+          path="/" element={
+            <>
+              {!user ? (
+                <Navigate to="/login" replace />
+              ) : movies.length === 0 ? (
+                <Col>The list is empty!</Col>
+              ) : (
+                <>
+                  {movies.map((movie) => (
+                    <Col className="mb-4" key={movie.id} md={3}>
+                      <MovieCard book={book} />
+                    </Col>
+                  ))}
+                </>
+              )}
+            </>
+          }
+        />
+      </Routes>
+    </Row>
+    </BrowserRouter>
+  );
+};
 
-    <>
-      <>
-        {movies.map((movie) => (
-          <MovieCard
-           key={movie.id}
-            movie={movie}
-           onMovieClick={(newSelectedMovie) => {
-             setSelectedMovie(newSelectedMovie);
-           }}
-          />
-        ))}
-      </>
-      <Button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</Button>
-    </>
-    )}
-  </Row>
-  )
-  };
--- components/movie-card/movie-card.jsx
+++ components/movie-card/movie-card.jsx
@@ -1,16 +1,18 @@
+import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie, onMovieClick}) => {
+export const MovieCard = ({ movie }) => {
     return (
-        <Card>
+        <Card className="h-100">
             <Card.Img variant="top" src={movie.image} />
             <Card.Body>
                 <Card.Title>{movie.title}</Card.Title>
-                <Card.Text>{useBootstrapBreakpoints.author}</Card.Text>
-            <Button onClick= {() => onMovieClick(movie)}
-            variant="link"> Open
-            </Button>
+                <Card.Text>{movie.director}</Card.Text>
+                <Link to={'/movies/${encodeURIComponent(movie.id)}'}>
+                    <Button variant="Link">Open</Button>
+                </Link>
             </Card.Body>
         </Card>
     
@@ -27,5 +29,4 @@ MovieCard.propTypes = {
         imagePath: PropTypes.string.isRequired
 
     }).isRequired,
-    onBookClick: PropTypes.func.isRequired
 };  
